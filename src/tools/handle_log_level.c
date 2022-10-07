f0db33834103ecf60e6821f7553a3578c07519a5
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-10-07 14:14:15 +0200

    Updated log level reader to catch more log levels.

--- src/tools/handle_log_level.c
+++ src/tools/handle_log_level.c
@@ -4,8 +4,10 @@
 // Wow, such logs, good level!
 //___________________________________________________________________________________________________________________________________________
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <syslog.h>
+#include <string.h>
 #include "slurm/slurm.h"
 #include "demeter.h"
 
@@ -15,15 +17,20 @@ bool handle_log_level(parsed_log_t *curr_log, demeter_conf_t *demeter_conf)
 
     if (curr_log->unparsed_log == NULL)
         return false;
-    if (strstr(curr_log->unparsed_log, "FATAL") != NULL)
+    if (strcasestr(curr_log->unparsed_log, "FATAL") != NULL ||
+    strcasestr(curr_log->unparsed_log, "fatal") != NULL)
         level_of_curr_log = FATAL;
-    if (strstr(curr_log->unparsed_log, "ERROR") != NULL)
+    if (strcasestr(curr_log->unparsed_log, "ERROR") != NULL ||
+    strcasestr(curr_log->unparsed_log, "kern err") != NULL)
         level_of_curr_log = ERROR;
-    if (strstr(curr_log->unparsed_log, "WARNING") != NULL)
+    if (strcasestr(curr_log->unparsed_log, "WARNING") != NULL ||
+    strcasestr(curr_log->unparsed_log, "kern warn") != NULL)
         level_of_curr_log = WARNING;
-    if (strstr(curr_log->unparsed_log, "INFO") != NULL)
+    if (strcasestr(curr_log->unparsed_log, "INFO") != NULL ||
+    strcasestr(curr_log->unparsed_log, "kern info") != NULL)
         level_of_curr_log = INFO;
-    if (strstr(curr_log->unparsed_log, "DEBUG") != NULL)
+    if (strcasestr(curr_log->unparsed_log, "DEBUG") != NULL ||
+    strcasestr(curr_log->unparsed_log, "kern debug") != NULL)
         level_of_curr_log = DEBUG;
     if (level_of_curr_log < demeter_conf->slurm_log_level) {
         if (curr_log->unparsed_log != NULL) {
